899e8349ec8c6832dd21063ddebb41640e8829a1
H-Matus <matusevicius.henrikas@gmail.com>
2023-06-25 23:13:50 +0100

    Removed unnecessary code for the clock project.

--- HSE_SYSCLK_8Mhz/Core/Src/main.c
+++ HSE_SYSCLK_8Mhz/Core/Src/main.c
@@ -13,36 +13,13 @@
 #define FALSE   0
 #define TRUE    1
 
-void SystemClockConfig(void);
 void UART2_Init(void);
 void Error_handler(void);
-uint8_t convert_to_capital(uint8_t);
-
-UART_HandleTypeDef huart2;
-
-char *user_data = "The application is running.\r\n";
-uint8_t data_buffer[100];
-uint8_t recvd_data;
-uint32_t count = 0;
-uint8_t reception_complete = FALSE;
 
 int main(void)
 {
 	HAL_Init();
-	SystemClockConfig();
 	UART2_Init();
-
-    uint16_t len_of_data = strlen(user_data);
-    
-    if( HAL_UART_Transmit(&huart2, (uint8_t*)user_data, len_of_data, HAL_MAX_DELAY) != HAL_OK )
-    {
-        Error_handler();
-    }
-
-    while(reception_complete != TRUE)
-    {
-        HAL_UART_Receive_IT(&huart2, &recvd_data, 1);
-    }
     
     while(1) { };
 
@@ -74,30 +51,4 @@ void UART2_Init(void)
 void Error_handler(void)
 {
     while(1);
-}
-
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
-{
-    if(recvd_data == '\r')
-    {
-        reception_complete = TRUE;
-        data_buffer[count++] = '\r';
-        HAL_UART_Transmit(&huart2, data_buffer, count, HAL_MAX_DELAY);
-    }
-    else
-    {
-        data_buffer[count++] = recvd_data;
-    }
-}
-
-uint8_t convert_to_capital(uint8_t data)
-{
-    /* ASCII manipulations: */
-    /* Checking if the letter is lowercase: */
-    if( data >= 'a' && data <= 'z' )
-    {
-        data = data - ('a' - 'A');
-    }
-
-    return data;
 }
