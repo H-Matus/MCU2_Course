dca3ab69de86b838962e293fb62db97e082ee565
H-Matus <matusevicius.henrikas@gmail.com>
2023-06-25 16:15:06 +0100

    Added MspInit()

--- Core/Src/msp.c
+++ Core/Src/msp.c
@@ -9,5 +9,15 @@
 
 void HAL_MspInit(void)
 {
-    /* Low level */
+    /* Low level processor specific inits.*/
+    /* 1. Set up the priority grouping of the ARM Cortex MX Processor. */
+    HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+
+    /* 2. Enable required system exceptions of the ARM Cortex MX Processor. */
+    SCB->SHCSR |= 0x7 << 16;
+
+    /* 3. Configure the priority for system exceptions. */
+    HAL_NVIC_SetPriority(MemoryManagement_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(BusFault_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(UsageFault_IRQn, 0, 0);
 }
