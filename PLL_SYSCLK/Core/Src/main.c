48ea7fc864f19010d38eae7528e71ea46dd70508
H-Matus <matusevicius.henrikas@gmail.com>
2023-06-26 02:10:32 +0100

    Finished PLL osc init and config.

--- PLL_SYSCLK/Core/Src/main.c
+++ PLL_SYSCLK/Core/Src/main.c
@@ -24,10 +24,9 @@ int main(void)
 
 	HAL_Init();
 
+    SystemClock_Config(SYS_CLOCK_FREQ_50_MHZ);
+
 	UART2_Init();
-
-    SystemClock_Config();
-
     
     while(1) { };
 
@@ -38,6 +37,8 @@ void SystemClock_Config(uint8_t clock_freq)
 {
     RCC_OscInitTypeDef osc_init;
     RCC_ClkInitTypeDef clk_init;
+
+    uint32_t FLatency = 0;
 
     /* Setting up HSI */
     osc_init.OscillatorType = RCC_OSCILLATORTYPE_HSI;
@@ -54,6 +55,14 @@ void SystemClock_Config(uint8_t clock_freq)
             osc_init.PLL.PLLN = 100;
             osc_init.PLL.PLLP = 2;
             osc_init.PLL.PLLQ = 2;
+
+            clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+            clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
+            clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+            clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+            clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
+
+            FLatency = FLASH_ACR_LATENCY_1WS;
             break;
         }
         case SYS_CLOCK_FREQ_84_MHZ:
@@ -62,6 +71,14 @@ void SystemClock_Config(uint8_t clock_freq)
             osc_init.PLL.PLLN = 168;
             osc_init.PLL.PLLP = 2;
             osc_init.PLL.PLLQ = 2;
+
+            clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+            clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
+            clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+            clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+            clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
+
+            FLatency = FLASH_ACR_LATENCY_2WS;
             break;
         }
         case SYS_CLOCK_FREQ_120_MHZ:
@@ -70,6 +87,14 @@ void SystemClock_Config(uint8_t clock_freq)
             osc_init.PLL.PLLN = 240;
             osc_init.PLL.PLLP = 2;
             osc_init.PLL.PLLQ = 2;
+
+            clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+            clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
+            clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+            clk_init.APB1CLKDivider = RCC_HCLK_DIV4;
+            clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
+
+            FLatency = FLASH_ACR_LATENCY_3WS;
             break;
         }
         default:
@@ -83,6 +108,15 @@ void SystemClock_Config(uint8_t clock_freq)
         Error_handler();
     }
     
+    if( HAL_RCC_ClockConfig(&clk_init, FLatency) != HAL_OK )
+    {
+        Error_handler();
+    }
+
+    /* SysTick configuration */
+    HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
+    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
 }
 
 void UART2_Init(void)
