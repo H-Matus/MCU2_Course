bb2ac3a4074967d932c4845021c8511068a065b9
H-Matus <matusevicius.henrikas@gmail.com>
2023-07-02 16:25:09 +0100

    Make a template for developing timers.

--- timer_IC_1/Core/Src/main_app.c
+++ timer_IC_1/Core/Src/main_app.c
@@ -2,48 +2,110 @@
 #include "stm32f4xx_hal.h"
 #include "main_app.h"
 
-void SystemClockConfig(void);
-void TIMER6_Init(void);
+void SystemClockConfig( uint8_t clock_freq );
 void GPIO_Init(void);
 void Error_handler(void);
 
-TIM_HandleTypeDef htimer6;
-
+TIM_HandleTypeDef htimer2;
 
 int main(void)
 {
 	HAL_Init();
+
 	SystemClockConfig();
+
 	GPIO_Init();
-	TIMER6_Init();
-	__HAL_RCC_BKPSRAM_CLK_ENABLE();
-	__HAL_RCC_PWR_CLK_ENABLE();
-	HAL_PWR_EnableBkUpAccess();
-	//Lets start timer
-	HAL_TIM_Base_Start(&htimer6);
 
-	while(1)
-	{
-		/* Loop until the update event flag is set */
-		while( ! (TIM6->SR & TIM_SR_UIF) );
-		/* The required time delay has been elapsed */
-		/* User code can be executed */
-		TIM6->SR = 0;
-		HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_5);
-
-	}
-
+    while(1);
 
 	return 0;
 }
 
 
-void SystemClockConfig(void)
+void SystemClockConfig( uint8_t clock_freq )
 {
+    RCC_OscInitTypeDef Osc_Init;
+    RCC_ClkInitTypeDef Clk_Init;
 
+    Osc_Init.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    Osc_Init.HSIState = RCC_HSI_ON;
+    Osc_Init.HSICalibrationValue = 16;
+    Osc_Init.PLL.PLLState = RCC_PLL_ON;
+    Osc_Init.PLL.PLLSource = RCC_PLLSOURCE_HSI;
 
+    switch(clock_freq)
+    {
+        case SYS_CLOCK_FREQ_50_MHZ:
+        {
+            osc_init.PLL.PLLM = 8;
+            osc_init.PLL.PLLN = 50;
+            osc_init.PLL.PLLP = RCC_PLLP_DIV2;
+            osc_init.PLL.PLLQ = 2;
+            osc_init.PLL.PLLR = 2;
+
+            clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+            clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+            clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+            clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+            clk_init.APB2CLKDivider = RCC_HCLK_DIV1;
+            break;
+        }
+        case SYS_CLOCK_FREQ_84_MHZ:
+        {
+            osc_init.PLL.PLLM = 8;
+            osc_init.PLL.PLLN = 84;
+            osc_init.PLL.PLLP = RCC_PLLP_DIV2;
+            osc_init.PLL.PLLQ = 2;
+            osc_init.PLL.PLLR = 2;
+
+            clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+            clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+            clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+            clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+            clk_init.APB2CLKDivider = RCC_HCLK_DIV1;
+            break;
+        }
+        case SYS_CLOCK_FREQ_120_MHZ:
+        {
+            osc_init.PLL.PLLM = 8;
+            osc_init.PLL.PLLN = 120;
+            osc_init.PLL.PLLP = RCC_PLLP_DIV2;
+            osc_init.PLL.PLLQ = 2;
+            osc_init.PLL.PLLR = 2;
+
+            clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+            clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+            clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+            clk_init.APB1CLKDivider = RCC_HCLK_DIV4;
+            clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
+            break;
+        }
+        default:
+        {
+            return;
+        }
+
+        if( HAL_RCC_OscConfig(&Osc_Init) != HAL_OK )
+        {
+            Error_handler();
+        }
+
+        if( HAL_RCC_ClockConfig(&Clk_Init, FLASH_LATENCY_2) != HAL_OK )
+        {
+            Error_handler();
+        }
+
+        /* Configure the sustick timer interrupt frequency (for every 1 ms) */
+        uint32_t hclk_freq = HAL_RCC_GetHCLKFreq();
+        HAL_SYSTICK_Config(hclk_freq / 1000);
+
+        /* Configure the systick */
+        HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+        /* SysTick_IRQn interrupot configuration */
+        HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+    }
 }
-
 
 void GPIO_Init(void)
 {
@@ -54,19 +116,6 @@ void GPIO_Init(void)
 	ledgpio.Pull = GPIO_NOPULL;
 	HAL_GPIO_Init(GPIOA,&ledgpio);
 }
-
-void TIMER6_Init(void)
-{
-	htimer6.Instance = TIM6;
-	htimer6.Init.Prescaler = 24;
-	htimer6.Init.Period = 64000-1;
-	if( HAL_TIM_Base_Init(&htimer6) != HAL_OK )
-	{
-		Error_handler();
-	}
-
-}
-
 
 void Error_handler(void)
 {
--- timer_IC_1/Core/Src/msp.c
+++ timer_IC_1/Core/Src/msp.c
@@ -23,17 +23,3 @@ void HAL_MspInit(void)
 	HAL_NVIC_SetPriority(BusFault_IRQn,0,0);
 	HAL_NVIC_SetPriority(UsageFault_IRQn,0,0);
 }
-
-void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htimer)
-{
-
-	//1. enable the clock for the TIM6 peripheral
-	__HAL_RCC_TIM6_CLK_ENABLE();
-
-	//2. Enable the IRQ of TIM6
-	HAL_NVIC_EnableIRQ(TIM6_DAC_IRQn);
-
-	//3. setup the priority for TIM6_DAC_IRQn
-	HAL_NVIC_SetPriority(TIM6_DAC_IRQn,15,0);
-
-}
