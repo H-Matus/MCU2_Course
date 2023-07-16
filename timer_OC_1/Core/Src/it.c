eb9e5b39d12dc86792b31b874d75d45bbacf1e17
H-Matus <matusevicius.henrikas@gmail.com>
2023-07-16 20:00:32 +0100

    Finished it.c template.

--- timer_OC_1/Core/Src/it.c
+++ timer_OC_1/Core/Src/it.c
@@ -5,4 +5,15 @@
  *      Author: Admin
  */
 
+#include "main.h"
 
+void SysTick_Handler(void)
+{
+    HAL_IncTick();
+    HAL_SYSTICK_IRQHandler();
+}
+
+void TIM2_IRQHandler(void)
+{
+    HAL_TIM_IRQHandler(&htimer2);
+}
