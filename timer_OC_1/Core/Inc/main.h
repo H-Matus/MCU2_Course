965efc880f8b8e56ca7746ef984f80141cc93b86
H-Matus <matusevicius.henrikas@gmail.com>
2023-07-16 19:38:30 +0100

    Add proper system clk freq definitions.

--- timer_OC_1/Core/Inc/main.h
+++ timer_OC_1/Core/Inc/main.h
@@ -9,8 +9,8 @@
 #define INC_MAIN_H_
 
 #define SYS_CLOCK_FREQ_50_MHZ 50
-#define SYS_CLOCK_FREQ_
-#define SYS_CLOCK_FREQ_
+#define SYS_CLOCK_FREQ_84_MHZ   84
+#define SYS_CLOCK_FREQ_120_MHZ  120
 
 
 #endif /* INC_MAIN_H_ */
